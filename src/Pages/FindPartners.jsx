61067b005caa80cac5e16829c8c2ae6ca39d7737
Shajid1041 <shajidmustakim906@gmail.com>
2025-12-12 04:55:14 +0600

    added new x logo

--- src/Pages/FindPartners.jsx
+++ src/Pages/FindPartners.jsx
@@ -2,7 +2,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router';
 import Loader from '../Components/Loader/Loader';
-import NotFounded from '../Components/NotFounded/NotFounded';
 
 const FindPartners = () => {
     const [partners, setPartners] = useState([]);
